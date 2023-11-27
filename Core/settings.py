c1509475088686c30dec325a48137ec3a4d303cd
Sharifjon <mominovsharif12@gmail.com>
2023-11-27 15:35:18 +0500

    for dias

--- Core/settings.py
+++ Core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'UserApp',
     'corsheaders',
     'CheckApp',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- UserApp/views.py
+++ UserApp/views.py
@@ -3,10 +3,13 @@ from .serializers import  UserSerializer,Login_SRL
 from .models import User
 from rest_framework.response import  Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 
 class RegistrationView(APIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @swagger_auto_schema(request_body=UserSerializer)
     def post(self,request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -19,6 +22,7 @@ class Login(APIView):
 # Create your views here.
     queryset = User.objects.all()
     serializer_class = Login_SRL
+    @swagger_auto_schema(request_body=Login_SRL)
     def post(self,request):
         name=request.data.get('name')
         password=request.data.get('password')
